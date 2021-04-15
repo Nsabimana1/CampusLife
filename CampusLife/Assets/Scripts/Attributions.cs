9bdf01af5b08680d333382a7b1b5b7a0baffaaf6
Cooper Grimes <grimescb@hendrix.edu>
2021-04-14 19:26:33 -0500

    Music changes
    Lowered music volume, made it loop, and added it to to a new attributions script

---
+++ CampusLife/Assets/Scripts/Attributions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attributions : MonoBehaviour
+{
+
+    /* Music
+     * Helice's Theme by Komiku https://freemusicarchive.org/music/Komiku/Helice_Awesome_Dance_Adventure_/helices-theme
+     */
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
